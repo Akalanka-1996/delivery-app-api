c276fa3fddb717eb13dd59ce42b8107cd5884f64
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-10-09 02:22:07 +0530

    delete lane added

--- controllers/laneController.js
+++ controllers/laneController.js
@@ -2,6 +2,12 @@ const asyncHandler = require('express-async-handler')
 const Lane = require('../models/lane.model')
 const Route = require('../models/route.model')
 
+
+const getLanes = asyncHandler(async (req, res) => {
+    const lanes = await Lane.find()
+    res.json(lanes)
+})
+
 const createLane = asyncHandler(async (req, res) => {
     const { lane, estimatedTime, routeId} = req.body
 
@@ -33,6 +39,22 @@ const createLane = asyncHandler(async (req, res) => {
     }
 })
 
+const deleteLane = asyncHandler(async (req, res) => {
+    const lane = await Lane.findById(req.params.id)
+    console.log(lane)
+
+    if (lane.user.toString() !== req.user._id.toString()) {
+        throw new Error("You cannot perform this action!")
+    }
+
+    if(lane) {
+        await lane.remove()
+        res.json({message: "Lane removed!"})
+    } else {
+        res.status(404)
+        throw new Error("Lane not found")
+    }
+})
 
 
-module.exports = { createLane }
+module.exports = { createLane, deleteLane, getLanes }
--- routes/lane.js
+++ routes/lane.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
-const {createLane} = require('../controllers/laneController')
+const {createLane, deleteLane, getLanes} = require('../controllers/laneController')
 
 const {protect} = require('../middleware/authMiddleware')
 
+router.route('/').get(protect, getLanes)
 router.route('/create').post(protect, createLane)
+router.route('/:id').delete(protect, deleteLane)
 
 module.exports = router
