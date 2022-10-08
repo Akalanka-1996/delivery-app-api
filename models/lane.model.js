fabfc2d9dc835fa3e3dce672a9ddc7b29678c806
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-10-09 01:22:33 +0530

    route and lane models added

---
+++ models/lane.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const laneSchema = mongoose.Schema({
+    lane: {
+        type: String
+    },
+    estimatedTime: {
+        type: Number
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"User"
+    },
+    routeId:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"Route"
+    }
+},{
+    timestamps:true
+})
+
+const Lane = mongoose.model("Lane", laneSchema)
+
+module.exports = Lane;
---
+++ models/route.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const routeSchema = mongoose.Schema({
+    area:{
+        type:String,
+    },
+    startPoint: {
+        type: String
+    },
+    startTime: {
+        type: Date
+    },
+    lanes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lane"
+    }],
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"User"
+    }
+},{
+    timestamps:true
+})
+
+const Route = mongoose.model("Route", routeSchema)
+
+module.exports = Route;
